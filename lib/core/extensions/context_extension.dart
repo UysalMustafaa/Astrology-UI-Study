8e33bccfb195551aec80276cadee39f0a3533a62
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2022-07-01 15:08:49 +0300

    Context extension in file updated.

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,7 +1,10 @@
+import 'package:astrology/core/theme/ITheme.dart';
+import 'package:astrology/core/theme/theme_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 extension ContextExtension on BuildContext {
   double dynamicHeight(double value) => MediaQuery.of(this).size.height * value;
   double dynamicWidth(double value) => MediaQuery.of(this).size.width * value;
+  ITheme appTheme() => ThemeViewModel.theme;
 }
