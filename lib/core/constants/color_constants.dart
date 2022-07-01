2fc2be2917acde58cb20754cf67feab577668c0d
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2022-07-01 15:08:12 +0300

    Color constants file added.

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color OuterSpace = Color(0xFF2C3639);
+  static const Color White = Color(0xffffffff);
+  static const Color Zanah = Color(0xFFD3EBCD);
+  static const Color RedBerry = Color(0xFF990000);
+  static const Color BattleshipGray = Color(0xFF809A6F);
+  static const Color JazzBerryJam = Color(0xFFA91079);
+  static const Color Malibu = Color(0xFF7FB5FF);
+}
