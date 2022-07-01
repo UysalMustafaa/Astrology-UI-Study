fe31c43a91eece36098e357893c01f4710172f17
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2022-07-01 15:11:27 +0300

    Dark theme file added.

---
+++ lib/core/theme/dark_theme.dart
@@ -0,0 +1,48 @@
+import 'package:astrology/core/theme/ITheme.dart';
+import 'package:flutter/material.dart';
+
+class DarkTheme extends ITheme {
+  static DarkTheme _instance;
+
+  static DarkTheme get instance {
+    if (_instance != null) return _instance;
+    _instance = DarkTheme._init();
+    return _instance;
+  }
+
+  DarkTheme._init();
+
+  final ThemeData _darkTheme = ThemeData.light();
+
+  @override
+  ThemeData get data => ThemeData(
+      appBarTheme: _darkTheme.appBarTheme,
+      colorScheme: _darkTheme.colorScheme,
+      inputDecorationTheme: _darkTheme.inputDecorationTheme.copyWith());
+
+  @override
+  double textScaleFactorFromFontSize({double fontSize, BuildContext context}) {
+    switch (fontSize?.toInt()) {
+      case 13:
+        {
+          return 0.0025;
+        }
+      case 14:
+        {
+          return 0.0025;
+        }
+      default:
+        {
+          return 0.0025;
+        }
+    }
+  }
+
+  @override
+  TextStyle get homePageHeaderTextStyle => throw UnimplementedError();
+
+  @override
+  double deviceScreenScale({double scaleFactor, BuildContext context}) {
+    throw UnimplementedError();
+  }
+}
