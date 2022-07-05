9dd478b6f5e35f4cd21770ad0663e54606f35724
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2022-07-05 16:22:37 +0300

    Card pick pages updated.

--- lib/pages/card_pick/card_pick_page.dart
+++ lib/pages/card_pick/card_pick_page.dart
@@ -1,7 +1,9 @@
 import 'package:astrology/core/constants/color_constants.dart';
+import 'package:astrology/core/widgets/custom_card.dart';
 import 'package:astrology/core/widgets/custom_page_scaffold.dart';
 import 'package:astrology/pages/card_pick/card_pick_second.dart';
 import 'package:astrology/core/extensions/context_extension.dart';
+import 'package:astrology/pages/card_pick/card_pick_three.dart';
 import 'package:flutter/material.dart';
 
 class CardPickPage extends StatelessWidget {
@@ -10,25 +12,62 @@ class CardPickPage extends StatelessWidget {
     return Scaffold(
       body: CustomPageScaffold(
         title: 'Kart Seç',
-        buttonTitle: 'Kartlara Sor',
-        body: Text(
-          'Bilmek istediğin sorunun cevabı belki de kartlarda yatıyor. Sorunu, kadim öğretilerden, sembollerden, mitler ve arketiplerden yola çıkarak hazırlanmış kartlara da sorabilirsin. Diyelim ki net bir sorun yok, o zaman da bugünün veya içinden geçtiğin dönemin sana mesajının ne olduğunu kartlara sorabilirsin. \n\nKarttaki mesajı alabilmen için biraz soyut ve sembolik düşünmen gerekiyor. Genelde çok analitik düşünen biriysen, buradaki mesajı bilinçli aklın yerine kalbinle duymaya çalışmanı öneriririz.',
-          style: context.appTheme().cardPickTextStyle,
+        buttonTitle: 'Kartlardan Öğren',
+        body: Column(
+          children: [
+            Text(
+              'Bilmek istediğin sorunun cevabı belki de kartlarda yatıyor. Sorunu, kadim öğretilerden, sembollerden, mitler ve arketiplerden yola çıkarak hazırlanmış kartlara da sorabilirsin. Diyelim ki net bir sorun yok, o zaman da bugünün veya içinden geçtiğin dönemin sana mesajının ne olduğunu kartlara sorabilirsin. \n\nKarttaki mesajı alabilmen için biraz soyut ve sembolik düşünmen gerekiyor. Genelde çok analitik düşünen biriysen, buradaki mesajı bilinçli aklın yerine kalbinle duymaya çalışmanı öneriririz.',
+              style: context.appTheme().cardPickTextStyle,
+            ),
+            sizedBoxItem(context),
+            Row(
+              children: [
+                Expanded(
+                  child: InkWell(
+                    child: CustomCard(
+                        title: 'Yükselen burç neden önemlidir?',
+                        onPress: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CardPickSecond(),
+                            ),
+                          );
+                        },
+                        buttonPress: true),
+                  ),
+                ),
+                sizedBoxItem(context),
+                Expanded(
+                  child: CustomCard(
+                      title: 'Astroloji hangi sorulara cevap vermez?',
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CardPickThree(),
+                          ),
+                        );
+                      },
+                      buttonPress: true),
+                ),
+              ],
+            ),
+          ],
         ),
         onBackPressed: () {
           Navigator.pop(context);
         },
-        onButtonPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CardPickSecond(),
-            ),
-          );
-        },
         pageColor: ColorConstants.BattleshipGray,
-        hasButton: true,
+        hasButton: false,
       ),
+    );
+  }
+
+  Widget sizedBoxItem(BuildContext context) {
+    return SizedBox(
+      height: context.dynamicHeight(0.025),
+      width: context.dynamicWidth(0.025),
     );
   }
 }
--- lib/pages/card_pick/card_pick_second.dart
+++ lib/pages/card_pick/card_pick_second.dart
@@ -9,11 +9,11 @@ class CardPickSecond extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomPageScaffold(
-        title: 'Cömertlik',
+        title: 'Yükselen Burç Neden Önemlidir?',
         buttonTitle: '',
         hasButton: false,
         body: Text(
-            'Bu kartı seçtiyseniz, hiç bir karşılık beklemeden bir şeyler vermeniz veya bir yardım yapmanız önerilir. Ne olduğu önemli değildir. Maddi bir değer olması gerekmez. Önemli olan kalbinizi karşılık beklemeden başkalarına açmanızdır.\n\nCömertlik, sizi kıtlık duygusundan ve bilincinden çekip çıkarır. Bolluk bilincine yönlendirir.',
+            'Soruların cevaplarını sırayla yazacağım ama önce “yükselen burç nedir” kısmıyla başlayalım. Bir anlığına kendinizi bir pasta olarak hayal edin. :) Bu pastanın içeriği kişisel gezegenlerin durumu ile renklenecek olsa da öz yapısı yani hamuru Güneş burcunuzdur. Ancak pastayı ilk gören kişi haliyle siz ona bir dilim sunmadıkça içini değil dış kaplamasını görecektir. İşte o dış kaplama da aslında bizim yükselen burcumuzdur. Bu pasta çok göz alıcı bir kaplamaya sahip ama sade lezzetli bir hamura sahip olabilir, ya da tam tersi çok sade bir kaplamaya sahip olup içerisinden bir sürü sürprizler çıkabilir. Yani özetle yükselen burç bizim kendimizi dış dünyaya yansıttığımız ve en önemlisi dış dünyadan nasıl algılandığımızı anlatır. Bir de pastayı kestiğimizde artık fındık mı çıkar, taş mı çıkar bilemeyeceğimiz en iç bölümü vardır ki bu da Ay burcudur. :) İnsanlar önce yükselen burcumuzu “görürler”, tanıdıkça “Güneş” ve sonra “Ay” burcumuzu algılamaya başlarlar.',
             style: context.appTheme().cardPickTextStyle),
         onBackPressed: () {
           Navigator.pop(context);
